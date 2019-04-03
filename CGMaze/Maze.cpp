b6c42e7b7ba002add1c6665c4de43e6eed4c97cf
Julia <jpichler@outlook.at>
2019-04-03 16:58:11 +0200

    Beginn der Implementierung der Maze (Spielfeld) Klasse

--- CGMaze/Maze.cpp
+++ CGMaze/Maze.cpp
@@ -1,3 +1,60 @@
+#include "Maze.h"
+#include "ppm.h"
 ´/**
  This file contains the main game board and game logic
  */
+
+
+
+	public Maze::Maze(ppmImage floorplan)
+{
+
+}
+	Maze::~Maze()
+{
+		
+}
+
+void Maze::roll(int speed)
+{
+	prevRoll = roll;
+	rollFlag = speed*rollRate;
+
+}
+
+void Maze::pitch(int speed)
+{
+
+	prevPitch = pitch;
+	pitchFlag = speed * pitchRate;
+}
+
+void Maze::resetMaze()
+{
+	pitch = 0;
+	roll = 0;
+	prevPitch = 0;
+	prevRoll = 0;
+	rollFlag = 0;
+	pitchFlag = 0;
+	ball_x = ballStart_x;
+	ball_y = ballStart_y;
+
+	ball_z = ballRadius; 
+	ballVelocity_x = ballVelocity_y = ballVelocity_z = 0;
+}
+
+void Maze::advance(time_t deltaTime)
+{
+	rotateField(deltaTime);
+	moveBall(deltaTime);
+	if (checkCollision)
+	{
+		handleCollision();
+	}
+}
+
+bool Maze::checkCollision()
+{
+	return false;
+}
--- CGMaze/Maze.h
+++ CGMaze/Maze.h
@@ -8,38 +8,48 @@ Header file for Maze.cpp
 #define pitchRate 10
 #define rollRate 10
 #define gravity 9.81
+#define ballRadius 1
+#define ballMass 1
 
 using namespace std;
 class Maze
 {
 	int width, height;
 	double ball_x, ball_y,ball_z;
-	double ballRadius, ballMass;
-	double pitch, roll;
+	int ballStart_x, ballStart_y;
+	double pitch, roll, prevPitch, prevRoll;
+	double rollFlag,pitchFlag;
 	double ballVelocity_x, ballVelocity_y, ballVelocity_z;
 	vector<Mesh> meshes;
+	int[] mazeGrid;
 
 	public Maze(ppmImage floorplan);
 	public ~Maze();
 	public vector<Mesh> getMeshes();
-	public void roll(int speed, time_t deltaTime);
-	public void pitch(int speed, time_t deltaTime);
-	public void reset();
-	public void advance();
+	public void roll(int speed);
+	public void pitch(int speed);
+	public void resetMaze();
+	public void advance(time_t deltaTime);
 	private bool checkCollision();
 	private void handleCollision();
-	private void rotateField();
-	private void moveBall();
+	private void rotateField(time_t deltaTime);
+	private void moveBall(time_t deltaTime);
 	public void getRotations(); //returns struct with rotation infos
+	private 
 };
 
 class Mesh
 {
 	double pos_x, pos_y, pos_z;
 	double width, height, depth;
+	vector<Vertex> vertexList;
 
 	public bool intersectsWithSphere(double pos_x, double pos_y, double pos_z, double radius);
+	public vector<Vertex> getVertices();
 
 
 };
-
+class Vertex
+{
+	public double x, y, z, u, v;
+};
