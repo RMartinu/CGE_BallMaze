0e17281357f8b3b74134a85c4187b24f374bbed6
Robert Martinu <robert.s.martinu@gmail.com>
2019-04-07 15:50:56 +0200

    A window to a brave new world

--- CGMaze/CGMaze.cpp
+++ CGMaze/CGMaze.cpp
@@ -9,10 +9,45 @@
 #include <GLFW/glfw3.h>
 
 
+void framebuffer_size_callback(GLFWwindow* window, int w, int h)
+{
+	glViewport(0,0, w,h);
+}
+
 int main()
 {
     std::cout << "Hello World!\n"; 
-	
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800,600,"Our first Window", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Something went wrong" << std::endl;
+		glfwTerminate();
+		return -11;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to init GLAD" << std::endl;
+		return -13;
+	}
+
+	glViewport(0,0,800,600);
+	glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 1;
+	 
 
 	
 }
